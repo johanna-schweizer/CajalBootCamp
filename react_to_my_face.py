3b8c241f471756bd5e3cf7b8aa62a3a864786077
johanna-schweizer <82204698+johanna-schweizer@users.noreply.github.com>
2021-10-15 17:48:00 +0200

    Update react_to_my_face.py

--- react_to_my_face.py
+++ react_to_my_face.py
@@ -95,7 +95,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                                 dim = (128,128)
 
                                 resized_img = cv2.resize(crop_image, dim, interpolation = cv2.INTER_AREA)
-                                resized_img = cv2.resize(crop_image, common.input_size(interpreter))
+                                resized_img = cv2.resize(resized_img, common.input_size(interpreter))
                                 # Send resized image to Coral
                                 common.set_input(interpreter, resized_img)
 
